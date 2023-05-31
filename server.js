d9e3ec1136d7ca718c75a51c4fd87031cf17ccc6
alyssar60819 <alyssar60819@gmail.com>
2023-05-30 23:20:17 -0700

    debugging

--- server.js
+++ server.js
@@ -5,49 +5,33 @@ const { MongoClient } = require('mongodb');
 const app = express();
 app.use(bodyParser.json());
 
+// Enable CORS
+app.use(function(req, res, next) {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Methods', 'GET, POST');
+  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+  next();
+});
+
 const uri = 'mongodb://localhost:27017'; // Update with your MongoDB connection string
 const client = new MongoClient(uri);
-let database; // Declare the database variable outside the route handlers
-
-async function connectToDatabase() {
-  try {
-    await client.connect();
-    database = client.db('readinglog');
-    console.log('Connected to MongoDB');
-  } catch (error) {
-    console.error('Error connecting to MongoDB:', error);
-    process.exit(1); // Exit the process if unable to connect to the database
-  }
-}
-
-connectToDatabase(); // Connect to the database when the server starts
 
 // API endpoint to add a new reading log entry
 app.post('/reading-log', async (req, res) => {
-  const entry = req.body;
+  // Code to add a new reading log entry
+  // ...
 
-  try {
-    const collection = database.collection('entries');
-    await collection.insertOne(entry);
-
-    res.status(201).json({ message: 'Reading log entry added successfully.' });
-  } catch (error) {
-    console.error('Error adding the entry:', error);
-    res.status(500).json({ message: 'An error occurred while adding the entry.' });
-  }
+  // Example response
+  res.status(201).json({ message: 'Reading log entry added successfully.' });
 });
 
 // API endpoint to retrieve all reading log entries
 app.get('/reading-log', async (req, res) => {
-  try {
-    const collection = database.collection('entries');
-    const entries = await collection.find().toArray();
+  // Code to retrieve all reading log entries
+  // ...
 
-    res.status(200).json(entries);
-  } catch (error) {
-    console.error('Error retrieving the entries:', error);
-    res.status(500).json({ message: 'An error occurred while retrieving the entries.' });
-  }
+  // Example response
+  res.status(200).json(entries);
 });
 
 // Start the server
